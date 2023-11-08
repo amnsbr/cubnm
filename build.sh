8b184d1cf6423f5bfce6dae1bed617e456e5aa8c
amnsbr <amnsbr@gmail.com>
2023-11-08 18:27:06 +0100

    build wo libks

--- build.sh
+++ build.sh
@@ -1,11 +1,10 @@
 # TODO: use MakeFile
 cd /data/project/ei_development/tools/cuBNM/cuBNM/cuda
 nvcc -c -rdc=true --compiler-options '-fPIC' -o bnm_tmp.o bnm.cu -I/data/project/ei_development/tools/gsl_build/include \
--I/data/project/ei_development/tools/libks/include -I/data/project/ei_development/tools/cuBNM/cuBNM/cpp
+-I/data/project/ei_development/tools/cuBNM/cuBNM/cpp
 nvcc -dlink --compiler-options '-fPIC' -o bnm.o bnm_tmp.o \
 /data/project/ei_development/tools/gsl_build_shared/lib/libgsl.a \
 /data/project/ei_development/tools/gsl_build_shared/lib/libgslcblas.a \
-/data/project/ei_development/tools/libks/libks.so \
 -lm -lcudart
 rm -f libbnm.a
 ar cru libbnm.a bnm.o bnm_tmp.o
