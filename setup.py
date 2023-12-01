b10baee240282076769c933771b5a00226a15914
amnsbr <amnsbr@gmail.com>
2023-12-01 11:53:13 +0100

    using pyproject.toml

--- setup.py
+++ setup.py
@@ -1,26 +1,36 @@
 from setuptools import setup, Extension, find_packages
-from setuptools.command.install import install
+from distutils.spawn import find_executable
+import subprocess
+
+# from setuptools.command.install import install
 import os
 import numpy as np
 
-"""
-Pre-requisites:
-
-GSL2.7
-> download and unzip & cd to directory
-> ./configure --prefix=<target_dir> --enable-shared
-> make && make install
-"""
-
-# specify if extension needs to be 
+# specify installation options
 many_nodes = os.environ.get("CUBNM_MANY_NODES") is not None
-gpu_enabled = os.environ.get("CUDA_VISIBLE_DEVICES") is not None # this probably only works on Juseless and JSC
-# Write the value of many_nodes to a temporary file
+
+
+# detect if there are GPUs
+def has_gpus():
+    if find_executable("nvidia-smi"):
+        try:
+            subprocess.check_output(["nvidia-smi"])
+            return True
+        except subprocess.CalledProcessError:
+            return False
+    else:
+        return False
+
+
+gpu_enabled = has_gpus()
+
+# Write the flags to a temporary _flags.py file
 with open("cuBNM/_flags.py", "w") as flag_file:
-    flag_file.write("\n".join(
-        [f"many_nodes_flag = {many_nodes}",
-        f"gpu_enabled_flag = {gpu_enabled}"]
-    ))
+    flag_file.write(
+        "\n".join(
+            [f"many_nodes_flag = {many_nodes}", f"gpu_enabled_flag = {gpu_enabled}"]
+        )
+    )
 
 os.environ["CC"] = "g++"
 os.environ["CXX"] = "g++"
@@ -28,25 +38,27 @@ os.environ["CXX"] = "g++"
 if gpu_enabled:
     print("GPU enabled")
     if many_nodes:
-        print("""
+        print(
+            """
         Installing with support for running simulations with high number of nodes 
         which uses __device__ instead of __shared__ memory for S_E history at t-1
         and therefore is slower, but with high N of nodes is needed because
         __shared__ memory is limited
-        """)
+        """
+        )
         libraries = ["m", "gomp", "bnm_many", "cudart"]
     else:
         libraries = ["m", "gomp", "bnm", "cudart"]
     bnm_ext = Extension(
-        'cuBNM.core',
-        ['cuBNM/cpp/run_simulations.cpp'],
-        language='c++',
-        extra_compile_args=[ # note that these are not the flags to compile bnm.cu
+        "cuBNM.core",
+        ["cuBNM/cpp/run_simulations.cpp"],
+        language="c++",
+        extra_compile_args=[  # note that these are not the flags to compile bnm.cu
             "-O3",
             "-m64",
             "-fopenmp",
             "-D NOISE_SEGMENT",
-            "-D GPU_ENABLED"
+            "-D GPU_ENABLED",
         ],
         libraries=libraries,
         extra_objects=[
@@ -54,18 +66,18 @@ if gpu_enabled:
             "/data/project/ei_development/tools/gsl_build_shared/lib/libgslcblas.a",
         ],
         include_dirs=[
-            '/data/project/ei_development/tools/gsl_build_shared/include', 
-            '/usr/lib/cuda/include',
-            '/usr/include/cuda',
+            "/data/project/ei_development/tools/gsl_build_shared/include",
+            "/usr/lib/cuda/include",
+            "/usr/include/cuda",
             np.get_include(),
-            ],
-        library_dirs = [".", '/usr/lib/cuda', 'cuBNM/cuda']
+        ],
+        library_dirs=[".", "/usr/lib/cuda", "cuBNM/cuda"],
     )
 else:
     bnm_ext = Extension(
-        'cuBNM.core',
-        ['cuBNM/cpp/run_simulations.cpp'],
-        language='c++',
+        "cuBNM.core",
+        ["cuBNM/cpp/run_simulations.cpp"],
+        language="c++",
         extra_compile_args=[
             "-O3",
             "-m64",
@@ -78,13 +90,9 @@ else:
             "/data/project/ei_development/tools/gsl_build_shared/lib/libgslcblas.a",
         ],
         include_dirs=[
-            '/data/project/ei_development/tools/gsl_build_shared/include', 
+            "/data/project/ei_development/tools/gsl_build_shared/include",
             np.get_include(),
-            ],
+        ],
     )
 
-setup(name = 'cuBNM', version = '1.0',
-    packages=find_packages(),
-    ext_modules = [bnm_ext])
-
-os.remove("cuBNM/_flags.py")
+setup(packages=find_packages(), ext_modules=[bnm_ext])
