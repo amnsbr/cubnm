4e9d7bd59aeb50be32bb4ece4a3bd442aa7ebdfe
Amin Saberi <amnsbr@gmail.com>
2023-12-06 08:46:53 +0100

    fix: setup cuda tools v 12.3

--- tools/cibw_before_all.sh
+++ tools/cibw_before_all.sh
@@ -2,5 +2,5 @@ arch=$(uname -m)
 # install miniconda: this is a faster approach for downloading nvidia components (instead of the entire toolkit) + gsl (instead of building it)
 curl -s -O https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-$arch.sh
 bash Miniconda3-latest-Linux-$arch.sh -b -p $HOME/miniconda && export PATH="$HOME/miniconda/bin:$PATH"
-conda install -y -c nvidia cuda-compiler=11.8 cuda-cudart-static=11.8 cuda-cudart-dev=11.8
+conda install -y -c nvidia cuda-compiler=12.3 cuda-cudart-static=12.3 cuda-cudart-dev=12.3
 conda install -y --no-deps -c conda-forge gsl=2.7
