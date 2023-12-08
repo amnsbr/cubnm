7c44332db51e4129e4995b50a6ec34c92dd9919e
amnsbr <amnsbr@gmail.com>
2023-12-08 17:49:48 +0100

    recommend source build with undefined symbol

--- src/cuBNM/_run_simulations.py
+++ src/cuBNM/_run_simulations.py
@@ -8,7 +8,11 @@ except ImportError as e:
     error_msg = str(e)
     if "GLIBC_2.29" in error_msg:
         print(error_msg)
-        print("To fix this error either update `ldd` or install cuBNM from source")
+        print("To fix this error either update `ldd` or build cuBNM from source (https://github.com/amnsbr/cuBNM)")
+        exit(1)
+    elif "undefined symbol" in error_msg:
+        print(error_msg)
+        print("To fix this error try building cuBNM from source (https://github.com/amnsbr/cuBNM)")
         exit(1)
     else:
         raise(e)
