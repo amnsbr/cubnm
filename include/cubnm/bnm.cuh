53e8d14f6b37a083924bbcd82dc6f92d3a7fdf34
amnsbr <amnsbr@gmail.com>
2024-11-08 10:32:52 +0100

    calculate delay on the fly in kernel [WIP]
    This will make larger number of delayed simulations possible as it won't require separate `delay` matrices per simulation. Also `max_delays` can be different across simulations and avoid waste of memory when velocities (and the size of the ring buffer that is needed) differ

--- include/cubnm/bnm.cuh
+++ include/cubnm/bnm.cuh
@@ -6,7 +6,8 @@ typedef void (*GlobalInputKernel)(
     u_real&, int&,
     const int&, const int&, const int&, 
     const int&, int&, int&, u_real**, 
-    int**, const bool&, const int&,
+    u_real**, const bool&, const int&,
+    int&, const u_real&,
     u_real**, u_real*
 );
 
@@ -14,7 +15,8 @@ __device__ void global_input_cond(
     u_real& tmp_globalinput, int& k_buff_idx,
     const int& nodes, const int& sim_idx, const int& SC_idx,
     const int& j, int& k, int& buff_idx, u_real** SC, 
-    int** delay, const bool& has_delay, const int& max_delay,
+    u_real** SC_dist, const bool& has_delay, const int& max_delay,
+    int& curr_delay, const u_real& velocity,
     u_real** conn_state_var_hist, u_real* conn_state_var_1
 );
 
@@ -22,7 +24,8 @@ __device__ void global_input_osc(
     u_real& tmp_globalinput, int& k_buff_idx,
     const int& nodes, const int& sim_idx, const int& SC_idx,
     const int& j, int& k, int& buff_idx, u_real** SC, 
-    int** delay, const bool& has_delay, const int& max_delay,
+    u_real** SC_dist, const bool& has_delay, const int& max_delay,
+    int& curr_delay, const u_real& velocity,
     u_real** conn_state_var_hist, u_real* conn_state_var_1
 );
 
