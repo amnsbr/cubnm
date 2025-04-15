0af0754eb8e0bbe986f86cc353e2897e07574568
Amin Saberi <amnsbr@gmail.com>
2025-04-15 11:14:09 +0200

    fix: cibuildwheel issue with ._ on Mac

--- tools/cibw_before_all.sh
+++ tools/cibw_before_all.sh
@@ -6,8 +6,5 @@ project=$1
 curl -s -O https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-$arch.sh
 bash Miniconda3-latest-Linux-$arch.sh -b -p /opt/miniconda
 conda install -y --no-deps -c conda-forge gsl=2.7
-# conda install -y -c nvidia cuda-nvcc=12.0 cuda-cccl=12.0 cuda-cudart=12.0 cuda-cudart-static=12.0 cuda-cudart-dev=12.0
-# if [[ ! -e /root/miniconda/bin/nvcc ]]; then
-#     echo "NVCC not found..Stopping build"
-#     exit 1
-# fi
+# get rid of "._" files created by MacOS (when calling cibuildwheel from MacOS)
+find /project -type f -name '._*' -delete
